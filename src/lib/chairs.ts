0197d25806d07f8fec3855265bce56c9d827429b
Niklas Tallund <niklastallund@gmail.com>
2026-01-07 13:43:29 +0100

    quick chair implementation

--- src/lib/chairs.ts
+++ src/lib/chairs.ts
@@ -6,7 +6,6 @@ import { api } from "./onslipClient";
   plus any labeles required to implement the extra functionality
   needed that is not provided by the tabs in the api. */
 
-
 export async function listAllChairs() {
   const chairs = await api.listTabs();
 
@@ -17,4 +16,40 @@ export async function getChair(chairId: number) {
   const chair = await api.getTab(chairId);
 
   return chair;
+}
+
+// orderId is the tableId the chair belongs to
+export async function createChair(name: string, orderId: number) {
+  const chair = await api.addTab({ name });
+  
+  // Link the chair (tab) to the table (order)
+  await api.updateOrder(orderId, {
+    resources: [chair.id]
+  });
+}
+
+export async function getTableChairs(tableId: number) {
+  const table = await api.getOrder(tableId);
+
+  if (!table) {
+    console.error(`Table with ID ${tableId} not found.`);
+    return [];
+  }
+
+  if (!table.resources) {
+    console.error(`No chairs found for table with ID ${tableId}.`);
+    return [];
+  }
+
+  const chairs = [];
+  // Fetch each chair associated with the table
+  for (const chairId of table.resources) {
+    const chair = await getChair(chairId);
+
+    if (chair) {
+      chairs.push(chair);
+    }
+  }
+
+  return chairs;
 }
